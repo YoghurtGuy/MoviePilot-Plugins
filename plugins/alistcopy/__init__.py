fe87158453b7e2c137743ffe7a889907d0687c69
yoghurt <41856917+YoghurtGuy@users.noreply.github.com>
2024-04-17 18:43:41 +0800

    Update __init__.py

--- plugins/alistcopy/__init__.py
+++ plugins/alistcopy/__init__.py
@@ -19,7 +19,7 @@ class AlistCopy(_PluginBase):
     # 插件图标
     plugin_icon = "webhook.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.0"
     # 插件作者
     plugin_author = "YoghurtGuy"
     # 作者主页
